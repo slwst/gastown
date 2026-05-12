d31e1d3e40bf5cafa8ecb4033b6f94ae148c4e11
slwst <email@slw.st>
2026-05-12 12:00:39 -0400

    chore: bump sha for v1.1.0 flake build

--- flake.nix
+++ flake.nix
@@ -26,9 +26,9 @@
         packages = {
           gt = pkgs.buildGoModule {
             pname = "gt";
-            version = "1.0.0";
+            version = "1.1.0";
             src = ./.;
-            vendorHash = "sha256-mJzpsl4XnIm3ZSg7fFn0MOdQQW1bdOkAJ+TikiLMXJM=";
+            vendorHash = "sha256-PQT/Xq9na3vI8Oy9INBYJf3GsiN5IxAVCxrNLhyIpO8=";
 
             ldflags = [
               "-X github.com/gastownhall/gastown/internal/cmd.Build=nix"
